d26691603c5854f4500843e02c4c82982f657a3d
Vladmir Parshikov <vladimirus@gmail.com>
2015-08-17 15:57:38 +0100

    added sample controller

---
+++ src/main/java/rs/web/IndexController.java
@@ -0,0 +1,15 @@
+package rs.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello() {
+        return "hello";
+    }
+}
