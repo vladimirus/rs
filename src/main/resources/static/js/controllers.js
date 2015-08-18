a8dee7e0816abc82e662d3693dd16e515f85f38a
Vladmir Parshikov <vladimirus@gmail.com>
2015-08-18 18:20:19 +0100

    added css

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,6 +1,11 @@
 function SearchController($scope, $location) {
     $scope.submit = function () {
-        $scope.query = 'search is not implemented yet :)'
+        //$scope.query = 'search is not implemented yet :)';
+        $scope.links = [
+            { uri: "http://google.com", text: $scope.query },
+            { uri: "http://google.com", text: "And another" },
+            { uri: "http://google.com", text: "Some more" }
+        ];
     };
 }
 
