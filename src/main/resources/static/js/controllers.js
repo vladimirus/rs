cb92b2b17309c98fcd7dc22302694a4cbacc62d5
Vladmir Parshikov <vladimirus@gmail.com>
2015-08-26 15:03:37 +0100

    added pace

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -2,6 +2,7 @@ function SearchController($scope, $stateParams, Search, $location) {
     $scope.submit = function () {
         console.log($scope.query);
         if ($scope.query) {
+            Pace.restart();
             var links = Search.query({query: $scope.query}, function () {
                 $scope.links = links;
             });
