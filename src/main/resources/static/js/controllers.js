8a0c88945fa02db7d2662ca1184173e539137ec3
Vladmir Parshikov <vladimirus@gmail.com>
2015-09-30 16:06:41 +0100

    keeping track of queries

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,13 +1,23 @@
 function SearchController($scope, $stateParams, Search, $location) {
+    if (!$scope.query) {
+        $scope.query = $location.search().q;
+    }
+
+    if ($scope.query) {
+        $scope.links = searchRs($scope.query);
+    }
+
     $scope.submit = function () {
-        console.log($scope.query);
         if ($scope.query) {
-            Pace.restart();
-            var links = Search.query({query: $scope.query}, function () {
-                $scope.links = links;
-            });
+            $scope.links = searchRs($scope.query);
         }
     };
+
+    function searchRs(query) {
+        $location.search('q', query);
+        Pace.restart();
+        return Search.query({query: query});
+    }
 }
 
 angular
