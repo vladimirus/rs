128e06d512d094241fbd5f19d0bff896be11a670
Vladmir Parshikov <vladimirus@gmail.com>
2015-10-08 14:03:53 +0100

    added js versioning

--- src/main/java/rs/web/IndexController.java
+++ src/main/java/rs/web/IndexController.java
@@ -1,15 +1,20 @@
 package rs.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    @Value("${rs.static.js.version:1}")
+    private Integer jsVersion;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("jsVersion", jsVersion);
         return "index";
     }
 }
--- src/test/java/rs/web/IndexControllerTest.java
+++ src/test/java/rs/web/IndexControllerTest.java
@@ -2,11 +2,16 @@ package rs.web;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.ui.Model;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexControllerTest {
@@ -15,11 +20,14 @@ public class IndexControllerTest {
 
     @Test
     public void shouldIndex()  {
+        // given
+        Model model = mock(Model.class);
 
         // when
-        String actual = indexController.index();
+        String actual = indexController.index(model);
 
         // then
         assertThat(actual, is("index"));
+        verify(model).addAttribute(eq("jsVersion"), any());
     }
 }
